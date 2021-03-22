2fed96fa222ad8be96b5bdd8095513d656a2bd8c
Ni4ka <astahova_nica@mail.ru>
2021-03-22 12:17:15 +0200

    Add file game_robot_X-Y.py

--- game_robot_X-Y.py
+++ game_robot_X-Y.py
@@ -1,15 +1,35 @@
 #!/usr/bin/env Python3
 
-roboX = 5
-roboY = 5
+#DATA
+
+roboX  = 5
+roboY  = 5
+charge = 100 #percents
+
+
+
+
+
+#Field parameters
+
+height  = int( input( "Insert the height of field: " ))
+width   = int( input( "Insert the width of field: " ))
+
+
+
+
+
+
+
+# ############# DRAWING THE MAP #######################
 
 ##################### Y loop #####################
 y = 1
-while y <= 10:
+while y <= width:
 
     ##################### X loop #####################
     x = 1
-    while x <= 10:
+    while x <= height:
 
         if roboX == x and roboY == y:
             print("R ", end="")
@@ -22,3 +42,33 @@ while y <= 10:
     print()    
     y += 1
 ##################### Y loop #####################
+
+# #################### CONTROLS #####################
+
+direction = input( "dir ( w/s/a/d/x ) > " )
+
+if( direction == "w" ):
+ roboY += 1
+ charge -= 5
+
+if( direction == "s" ):
+ roboY -= 1
+ charge -= 5
+
+if( direction == "a" ):
+ roboX -= 1
+ charge -= 5
+
+if( direction == "d" ):
+ roboX += 1
+ charge -= 5
+
+if( direction == "x" ):
+ system( "clear" )
+ print( "Thank you for playing!" )
+# break
+
+
+
+
+
